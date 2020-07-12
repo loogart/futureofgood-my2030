8477849b2bf641a1c65d51587db85bf476a0c749
Chris Soueidan <chris@loogart.com>
2020-07-12 18:01:22 -0400

    fix for bodymovin on chrome
    issue with global css property from the Future of Good website:
    *,
    :after,
    :before {
        transition: none;
    }

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+  var animation1 = bodymovin.loadAnimation({
+      container: document.getElementById('bm1'),
+      path: 'js/1bailey.json',
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      name: "Bailey"
+  })
+  animation1.setSubframe(false);
+
+  var animation2 = bodymovin.loadAnimation({
+      container: document.getElementById('bm2'),
+      path: 'js/2cheyenne.json',
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      name: "Cheyenne"
+  })
+  animation2.setSubframe(false);
+
+  var animation3 = bodymovin.loadAnimation({
+      container: document.getElementById('bm3'),
+      path: 'js/3lliam.json',
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      name: "Lliam"
+  })
+  animation3.setSubframe(false);
+
+  var animation3 = bodymovin.loadAnimation({
+      container: document.getElementById('bm4'),
+      path: 'js/4luna.json',
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      name: "Luna"
+  })
+  animation4.setSubframe(false);
